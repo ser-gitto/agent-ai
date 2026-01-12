2add3c485717ef67fc0c3fc99b61b5621f674517
ser-gitto <sergiusz.jaczminski@gmail.com>
2026-01-13 00:15:24 +0100

    Update sttest.py

--- sttest.py
+++ sttest.py
@@ -5,14 +5,15 @@ client = OpenAI()
 MODEL = "gpt-4o-mini"
 
 SYSTEM_PROMPT = """
-Jesteś autonomicznym agentem AI.
-Dostajesz CEL użytkownika i realizujesz go krok po kroku.
+Jesteś autonomicznym agentem AI.Udajesz trenera fitnes. Wypytujesz użytkownika o wiek, wagę i inne parametry potrzebne do stworzenia, diety i planu treningowego.Pytasz go również o cel,np wagę docelową.
+Powyższe informacje dostajesz jako  CEL użytkownika i realizujesz go krok po kroku.
 
 Zasady:
-- Najpierw tworzysz PLAN
+- Najpierw tworzysz PLAN ćwiczeń z rozpiską dzień po dniu.
 - Wykonujesz JEDEN krok naraz
 - Po każdym kroku oceniasz, czy cel jest zrealizowany
 - Odpowiadasz po polsku
+- następnie przygotowujesz dietę na 4 tygodnie
 """
 
 def call_llm(messages):
@@ -25,7 +26,7 @@ def call_llm(messages):
 def create_plan(goal):
     return call_llm([
         {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": f"CEL: {goal}\nStwórz plan krok po kroku."}
+        {"role": "user", "content": f"CEL: {goal}\nStwórz plan treningowy krok po kroku, a nastepnie diete rozpisaną na kilka tygodni"}
     ])
 
 def execute_step(goal, plan, step):
@@ -61,7 +62,7 @@ Odpowiedz tylko: TAK lub NIE.
 # ===== STREAMLIT UI =====
 
 st.set_page_config(page_title="Autonomiczny Agent AI", page_icon="🤖")
-st.title("🤖 Autonomiczny Agent AI")
+st.title("🤖 Fit Sergio Trener AI")
 
 goal = st.text_area("🎯 Podaj cel dla agenta:")
 
@@ -89,4 +90,5 @@ if st.button("🚀 START") and goal:
             st.success("✅ CEL ZREALIZOWANY")
             break
     else:
-        st.warning("⚠️ Plan wykonany, ale cel może być niepełny")
+
+        st.warning("⚠️ Plan wykonany, ale by osiągnąć cel potrzebne jest twoje zaangażowanie a nie lecenie w ciula")
